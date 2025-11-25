2028f96fb8e1b8a1da1c977bc0c97f12b4dceb43
Maksim <ignatov.maksim.04@mail.ru>
2025-11-25 23:40:23 +0300

    feat: add env & optimized hikari, OrderPayer

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -41,7 +41,7 @@ class OrderPayer(registry: MeterRegistry) {
         64,
         0L,
         TimeUnit.MILLISECONDS,
-        LinkedBlockingQueue(256),
+        LinkedBlockingQueue(5000),
         NamedThreadFactory("payment-submission-executor"),
         ThreadPoolExecutor.AbortPolicy()
     )
@@ -62,7 +62,7 @@ class OrderPayer(registry: MeterRegistry) {
             .register(registry)
     }
 
-    private val ingressRate = 100
+    private val ingressRate = 1000
     private val limiter = TokenBucketRateLimiter(
         rate = ingressRate,
         bucketMaxCapacity = ingressRate * 10,
